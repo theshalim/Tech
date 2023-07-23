a52671c9f06bfa120c537be1bb6c2670053871e6
Reza <shalim.reza@yahoo.com>
2023-07-23 22:54:19 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tech.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
