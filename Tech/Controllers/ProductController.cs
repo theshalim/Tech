c5feef0064a90efc544e013df4d35250e8f67ec1
Reza <shalim.reza@yahoo.com>
2023-07-25 00:13:47 +0600

    Create Page Design

--- Tech/Controllers/ProductController.cs
+++ Tech/Controllers/ProductController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Tech.Data;
+using Tech.Models;
 
 namespace Tech.Controllers
 {
@@ -19,6 +21,25 @@ namespace Tech.Controllers
         {
             return View(await _context.products.ToListAsync());
             
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewData["ColorId"] = new SelectList(_context.colors, "ColorId", "ColorId");
+            ViewData["UnitId"] = new SelectList(_context.productUnits, "UnitId", "UnitId");
+            ViewData["ManufracturingAddressId"] = new SelectList(_context.manufracturingAddresses, "ManufracturingAddressId", "ManufracturingAddressId");
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
         }
     }
 }
